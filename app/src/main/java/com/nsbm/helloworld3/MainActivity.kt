23bf02aaecf0e68b4cf17c8e4f37749195f012ce
Amila Indrajith Karunarathna <amilai@itx360.com>
2020-03-09 20:46:11 +0530

    Initial commit

---
+++ app/src/main/java/com/nsbm/helloworld3/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.nsbm.helloworld3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private var etUsernameM : EditText? = null
+    private var etPasswordM : EditText? = null
+    private var btnButtonM : Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        etUsernameM = findViewById(R.id.etUsername)
+        etPasswordM = findViewById(R.id.etPassword)
+        btnButtonM = findViewById(R.id.btnLogin)
+
+        btnButtonM?.setOnClickListener {
+
+            if(etUsernameM?.text.toString().isEmpty() || etPasswordM?.text.toString().isEmpty()) {
+                Toast.makeText(applicationContext, "Please enter username and password", Toast.LENGTH_SHORT).show()
+            }else{
+                var username = etUsernameM?.text.toString()
+                var password = etPasswordM?.text.toString()
+                Toast.makeText(applicationContext, "UN: "+username+" PW: "+password, Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, MovieListActivity::class.java).apply {}
+                startActivity(intent)
+            }
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/nsbm/helloworld3/MovieListActivity.kt
@@ -0,0 +1,12 @@
+package com.nsbm.helloworld3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_list)
+    }
+}
