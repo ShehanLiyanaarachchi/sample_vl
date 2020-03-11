18981a8953c6bd49537d43f5442ca68582722c88
Amila Indrajith Karunarathna <amilai@itx360.com>
2020-03-11 14:21:31 +0530

    Initial commit

---
+++ app/src/main/java/com/nsbm/helloworld3/Movie.kt
@@ -0,0 +1,3 @@
+package com.nsbm.helloworld3
+
+data class Movie (var title: String, var image: String)
--- app/src/main/java/com/nsbm/helloworld3/MovieListActivity.kt
+++ app/src/main/java/com/nsbm/helloworld3/MovieListActivity.kt
@@ -2,11 +2,42 @@ package com.nsbm.helloworld3
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MovieListActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var recyclerAdapter: RecyclerMovieAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_list)
+
+        recyclerView = findViewById(R.id.recyclerview)
+        recyclerAdapter = RecyclerMovieAdapter(this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = recyclerAdapter
+
+
+        val apiInterface = ApiInterface.create().getMovies()
+
+        //apiInterface.enqueue( Callback<List<Movie>>())
+        apiInterface.enqueue( object : Callback<List<Movie>> {
+            override fun onResponse(call: Call<List<Movie>>?, response: Response<List<Movie>>?) {
+
+                if(response?.body() != null)
+                    recyclerAdapter.setMovieListItems(response.body()!!)
+            }
+
+            override fun onFailure(call: Call<List<Movie>>?, t: Throwable?) {
+
+            }
+        })
+
+
     }
 }
